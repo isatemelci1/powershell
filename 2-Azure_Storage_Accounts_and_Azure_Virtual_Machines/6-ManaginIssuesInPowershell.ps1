c2283f2d319ba3e8b4a86c4f23092501f760f754
isatemelci1 <isatemelci1@gmail.com>
2022-06-22 22:51:44 +0200

    manage issues in powershell debug script added

--- 2-Azure_Storage_Accounts_and_Azure_Virtual_Machines/6-ManaginIssuesInPowershell.ps1
+++ 2-Azure_Storage_Accounts_and_Azure_Virtual_Machines/6-ManaginIssuesInPowershell.ps1
@@ -6,5 +6,9 @@ $StorageAccount=New-AzStorageAccount -ResourceGroupName $ResourceGroupName `
 -Name $AccountName -Location $Location -SkuName 'Standard_LRS' `
 -Kind "StorageV2" -Debug 
 
+$StorageAccount
+
 $StorageAccount=Get-AzStorageAccount -ResourceGroupName $ResourceGroupName `
 -Name $AccountName
+
+$StorageAccount
