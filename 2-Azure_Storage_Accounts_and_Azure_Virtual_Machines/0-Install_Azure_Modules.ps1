02a3ee413b5012aac711df843351c6c3b37c1f83
isatemelci1 <isatemelci1@gmail.com>
2022-06-22 22:46:27 +0200

    Install azure module script added

--- 2-Azure_Storage_Accounts_and_Azure_Virtual_Machines/0-Install_Azure_Modules.ps1
+++ 2-Azure_Storage_Accounts_and_Azure_Virtual_Machines/0-Install_Azure_Modules.ps1
@@ -2,4 +2,4 @@ Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 
 Install-Module -Name Az -Scope CurrentUser -Repository PSGallery -Force -AllowClobber
 
-Connect-AzAccount
+Connect-AzAccoun
