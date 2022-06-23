5bc5cda44ce8fab0c80980792c3504fcfab09ceb
isatemelci1 <isatemelci1@gmail.com>
2022-06-23 23:42:36 +0200

    invoke web requests script added

---
+++ 2-Azure_Storage_Accounts_and_Azure_Virtual_Machines/31-InvokeWebRequests.ps1
@@ -0,0 +1,13 @@
+<#
+Command Reference
+
+1. Invoke-WebRequest
+https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/invoke-webrequest?view=powershell-7.2
+
+#>
+
+$Uri ="https://azure.microsoft.com/en-us/account/"
+
+$Request=Invoke-WebRequest -Uri $Uri
+
+'The status code is ' + $Request.StatusCode
